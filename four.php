40ac4de031b5448805c604ede6e806da8cb22de1
anutka0306 <annakotelnikova4@gmail.com>
2019-10-23 20:19:56 +0300

    lesson 1

---
+++ four.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Lesson number 4";
+$title_h1 = "Hello from here";
+$date = date('Y/m/d');
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?=$title?></title>
+</head>
+<body>
+<h1><?=$title_h1?></h1>
+<h3>Today is: <?=$date?></h3>
+</body>
+</html>
